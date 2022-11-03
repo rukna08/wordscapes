576506e9dc3bb209596e295d1d173d6ae3ec2026
Ankur Banerjee <rukna2003@gmail.com>
2022-11-03 23:30:17 +0530

    -

---
+++ Assets/Scripts/MoveRightAndSpawnAtLeft.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class MoveRightAndSpawnAtLeft : MonoBehaviour {
+    void Update() {
+        transform.position += new Vector3(.5f, 0f, 0f);
+
+        if (transform.position.x > Screen.width + 250) {
+            transform.position = new Vector2(0f, transform.position.y);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Quit.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Quit : MonoBehaviour {
+    
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            Application.Quit();
+        }
+    }
+}
